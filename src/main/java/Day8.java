347af3c324e57d7ac6d97414aa2c776a7ffab39c
Eugene Grybinnyk <eugeneg@backbase.com>
2024-12-08 16:26:26 +0100

    Day 8 (refactored)

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -24,6 +24,84 @@ public class Day8 extends Day {
     }
 
 
+    @Override
+    public String part1(String input) {
+        char[][] map = buildMap(input);
+        Map<Character, List<Antenna>> groups = locateAntennas(map)
+            .stream()
+            .collect(Collectors.groupingBy(Antenna::frequency));
+
+        Set<Antinode> antinodes = placeAntinodes(map, groups, false);
+
+        return String.valueOf(antinodes.size());
+    }
+
+    @Override
+    public String part2(String input) {
+        char[][] map = buildMap(input);
+        Map<Character, List<Antenna>> groups = locateAntennas(map)
+            .stream()
+            .collect(Collectors.groupingBy(Antenna::frequency));
+
+        Set<Antinode> antinodes = placeAntinodes(map, groups, true);
+
+        return String.valueOf(antinodes.size());
+    }
+
+    private Set<Antinode> placeAntinodes(char[][] map, Map<Character, List<Antenna>> groups, boolean fullLine) {
+        Set<Antinode> antinodes = new AntinodeHashSet(map[0].length, map.length);
+        for (List<Antenna> group : groups.values()) {
+            for (int i = 0; i < group.size() - 1; i++) {
+                for (int j = i + 1; j < group.size(); j++) {
+                    Antenna a1 = group.get(i);
+                    Antenna a2 = group.get(j);
+                    int deltaY = Math.abs(a1.y() - a2.y());
+                    int deltaX = Math.abs(a1.x() - a2.x());
+
+                    if (a1.x() > a2.x()) {
+                        Antenna b = a1;
+                        a1 = a2;
+                        a2 = b;
+                    }
+
+                    int left = fullLine ? 0 : 1;
+                    do {
+                        left++;
+                    } while (antinodes.add(new Antinode(
+                        a2.x() - deltaX * left,
+                        a2.y() < a1.y() ? a2.y() + deltaY * left : a2.y() - deltaY * left
+                    )) && fullLine);
+
+                    int right = fullLine ? 0 : 1;
+                    do {
+                        right++;
+                    } while (antinodes.add(new Antinode(
+                        a1.x() + deltaX * right,
+                        a2.y() < a1.y() ? a1.y() - deltaY * right : a1.y() + deltaY * right
+                    )) && fullLine);
+                }
+            }
+        }
+        return antinodes;
+    }
+
+    private char[][] buildMap(String input) {
+        return input.lines().map(String::toCharArray).toArray(char[][]::new);
+    }
+
+    private List<Antenna> locateAntennas(char[][] map) {
+        List<Antenna> antennas = new ArrayList<>();
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+                if (map[y][x] == '.') {
+                    continue;
+                }
+                antennas.add(new Antenna(x, y, map[y][x]));
+            }
+        }
+        return antennas;
+    }
+
     record Antenna(int x, int y, char frequency) {
 
     }
@@ -50,111 +128,5 @@ public class Day8 extends Day {
             super.add(a);
             return true;
         }
-    }
-
-    @Override
-    public String part1(String input) {
-        char[][] map = input.lines().map(String::toCharArray).toArray(char[][]::new);
-
-        List<Antenna> antennas = new ArrayList<>();
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == '.') {
-                    continue;
-                }
-                antennas.add(new Antenna(x, y, map[y][x]));
-            }
-        }
-
-        Map<Character, List<Antenna>> groups = antennas.stream().collect(Collectors.groupingBy(Antenna::frequency));
-
-        Set<Antinode> antinodes = new AntinodeHashSet(map[0].length, map.length);
-
-        for (List<Antenna> group : groups.values()) {
-            for (int i = 0; i < group.size() - 1; i++) {
-                for (int j = i + 1; j < group.size(); j++) {
-                    Antenna a1 = group.get(i);
-                    Antenna a2 = group.get(j);
-                    int deltaY = Math.abs(a1.y() - a2.y());
-                    int deltaX = Math.abs(a1.x() - a2.x());
-
-                    if (a1.x() > a2.x()) {
-                        Antenna b = a1;
-                        a1 = a2;
-                        a2 = b;
-                    }
-
-                    antinodes.add(new Antinode(
-                        a1.x() - deltaX,
-                        a2.y() < a1.y() ? a1.y() + deltaY : a1.y() - deltaY
-                    ));
-                    antinodes.add(new Antinode(
-                        a2.x() + deltaX,
-                        a2.y() < a1.y() ? a2.y() - deltaY : a2.y() + deltaY
-                    ));
-
-                }
-            }
-        }
-
-        return String.valueOf(antinodes.size());
-    }
-
-    @Override
-    public String part2(String input) {
-        char[][] map = input.lines().map(String::toCharArray).toArray(char[][]::new);
-
-        List<Antenna> antennas = new ArrayList<>();
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == '.') {
-                    continue;
-                }
-                antennas.add(new Antenna(x, y, map[y][x]));
-            }
-        }
-
-        Map<Character, List<Antenna>> groups = antennas.stream().collect(Collectors.groupingBy(Antenna::frequency));
-
-        Set<Antinode> antinodes = new AntinodeHashSet(map[0].length, map.length);
-
-        for (List<Antenna> group : groups.values()) {
-            for (int i = 0; i < group.size() - 1; i++) {
-                for (int j = i + 1; j < group.size(); j++) {
-                    Antenna a1 = group.get(i);
-                    Antenna a2 = group.get(j);
-                    int deltaY = Math.abs(a1.y() - a2.y());
-                    int deltaX = Math.abs(a1.x() - a2.x());
-
-                    if (a1.x() > a2.x()) {
-                        Antenna b = a1;
-                        a1 = a2;
-                        a2 = b;
-                    }
-
-                    int left = 0;
-                    do {
-                        left++;
-                    } while (antinodes.add(new Antinode(
-                        a1.x() - deltaX * left,
-                        a2.y() < a1.y() ? a1.y() + deltaY * left : a1.y() - deltaY * left
-                    )));
-
-                    antinodes.add(new Antinode(a1.x(), a1.y()));
-
-                    int right = 0;
-                    do {
-                        right++;
-                    } while (antinodes.add(new Antinode(
-                        a2.x() + deltaX * right,
-                        a2.y() < a1.y() ? a2.y() - deltaY * right : a2.y() + deltaY * right
-                    )));
-
-                    antinodes.add(new Antinode(a2.x(), a2.y()));
-                }
-            }
-        }
-
-        return String.valueOf(antinodes.size());
     }
 }
