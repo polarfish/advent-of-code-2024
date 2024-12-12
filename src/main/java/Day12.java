06b3357391269327810679b7670f49fea65e31ba
Eugene Grybinnyk <eugeneg@backbase.com>
2024-12-12 11:46:39 +0100

    Day 12 part 2

--- src/main/java/Day12.java
+++ src/main/java/Day12.java
@@ -8,14 +8,19 @@ public class Day12 extends Day {
 
         String sample = readFile("%s_sample.txt".formatted(day.name()));
         String sample2 = readFile("%s_sample2.txt".formatted(day.name()));
+        String sample3 = readFile("%s_sample3.txt".formatted(day.name()));
+        String sample4 = readFile("%s_sample4.txt".formatted(day.name()));
         String full = readFile("%s.txt".formatted(day.name()));
 
-        assertEquals(140, day.part1(sample2));
         assertEquals(1930, day.part1(sample));
+        assertEquals(140, day.part1(sample2));
         assertEquals(1467094, day.part1(full));
 
-        assertEquals(0, day.part2(sample));
-        assertEquals(0, day.part2(full));
+        assertEquals(1206, day.part2(sample));
+        assertEquals(80, day.part2(sample2));
+        assertEquals(236, day.part2(sample3));
+        assertEquals(368, day.part2(sample4));
+        assertEquals(881182, day.part2(full));
 
         day.run(full, day::part1, "Part 1 result");
         day.run(full, day::part2, "Part 2 result");
@@ -26,7 +31,7 @@ public class Day12 extends Day {
 
     @Override
     public String part1(String input) {
-        char[][] map = input.lines().map(String::toCharArray).toArray(char[][]::new);
+        char[][] map = parseMap(input);
 
         Set<Integer> processed = new HashSet<>(map.length * map[0].length);
         int result = 0;
@@ -44,7 +49,24 @@ public class Day12 extends Day {
 
     @Override
     public String part2(String input) {
-        return "0";
+        char[][] map = parseMap(input);
+
+        Set<Integer> processed = new HashSet<>(map.length * map[0].length);
+        int result = 0;
+        int[] region;
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+                if ((region = measureRegion2(map, x, y, processed)) != null) {
+                    result += region[AREA] * region[PERIMETER];
+                }
+            }
+        }
+
+        return String.valueOf(result);
+    }
+
+    private static char[][] parseMap(String input) {
+        return input.lines().map(String::toCharArray).toArray(char[][]::new);
     }
 
     private int[] measureRegion(char[][] map, int x, int y, Set<Integer> processed) {
@@ -89,6 +111,64 @@ public class Day12 extends Day {
             measureRegion(map, x - 1, y, processed, plant, region);
         } else {
             region[PERIMETER]++;
+        }
+    }
+
+    private int[] measureRegion2(char[][] map, int x, int y, Set<Integer> processed) {
+        if (processed.contains(toPointId(x, y))) {
+            return null;
+        }
+        int[] region = new int[2];
+        measureRegion2(map, x, y, processed, map[y][x], region);
+        return region;
+    }
+
+    private void measureRegion2(char[][] map, int x, int y, Set<Integer> processed, char plant, int[] region) {
+        if (processed.contains(toPointId(x, y))) {
+            return;
+        }
+        region[AREA]++;
+        processed.add(toPointId(x, y));
+
+        // up
+        if (y > 0 && map[y - 1][x] == plant) {
+            measureRegion2(map, x, y - 1, processed, plant, region);
+        } else {
+            region[PERIMETER]++;
+            if (x > 0 && map[y][x - 1] == plant && (y == 0 || map[y - 1][x - 1] != plant)) {
+                region[PERIMETER]--;
+            }
+        }
+
+        // right
+        if (x < map[0].length - 1 && map[y][x + 1] == plant) {
+            measureRegion2(map, x + 1, y, processed, plant, region);
+        } else {
+            region[PERIMETER]++;
+            if (y > 0 && map[y - 1][x] == plant && (x == map[0].length - 1 || map[y - 1][x + 1] != plant)) {
+                region[PERIMETER]--;
+            }
+        }
+
+        // down
+        if (y < map.length - 1 && map[y + 1][x] == plant) {
+            measureRegion2(map, x, y + 1, processed, plant, region);
+        } else {
+            region[PERIMETER]++;
+            if (x < map[0].length - 1 && map[y][x + 1] == plant && (y == map.length - 1
+                || map[y + 1][x + 1] != plant)) {
+                region[PERIMETER]--;
+            }
+        }
+
+        // left
+        if (x > 0 && map[y][x - 1] == plant) {
+            measureRegion2(map, x - 1, y, processed, plant, region);
+        } else {
+            region[PERIMETER]++;
+            if (y < map.length - 1 && map[y + 1][x] == plant && (x == 0 || map[y + 1][x - 1] != plant)) {
+                region[PERIMETER]--;
+            }
         }
     }
 
