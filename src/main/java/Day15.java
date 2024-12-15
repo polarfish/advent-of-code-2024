06edf1768d7cdb6dfae2853fa364b1c65f5be641
Eugene Grybinnyk <eugeneg@backbase.com>
2024-12-15 21:09:28 +0100

    Day 15 (refactoring)

--- src/main/java/Day15.java
+++ src/main/java/Day15.java
@@ -1,5 +1,3 @@
-import java.util.stream.IntStream;
-
 public class Day15 extends Day {
 
     public static void main(String[] args) {
@@ -22,27 +20,14 @@ public class Day15 extends Day {
         day.run(full, day::part2, "Part 2 result");
     }
 
-    public static final int UP = 0;
-    public static final int RIGHT = 1;
-    public static final int DOWN = 2;
-    public static final int LEFT = 3;
-
     @Override
     public String part1(String input) {
         String[] split = input.split("\n\n");
-        int[][] map = parseMap(split[0]);
-        int[] moves = parseMoves(split[1]);
-
-        int robotX = -1;
-        int robotY = -1;
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == '@') {
-                    robotX = x;
-                    robotY = y;
-                }
-            }
-        }
+        char[][] map = parseMap(split[0]);
+        char[] moves = split[1].replaceAll("\n", "").toCharArray();
+        int[] robot = locateRobot(map);
+        int robotX = robot[0];
+        int robotY = robot[1];
 
         for (int dir : moves) {
             int x = robotX;
@@ -67,14 +52,7 @@ public class Day15 extends Day {
             }
         }
 
-        int result = 0;
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == 'O') {
-                    result += y * 100 + x;
-                }
-            }
-        }
+        int result = sumUpCoordinates(map, 'O');
 
         return String.valueOf(result);
     }
@@ -82,19 +60,11 @@ public class Day15 extends Day {
     @Override
     public String part2(String input) {
         String[] split = input.split("\n\n");
-        int[][] map = parseWideMap(split[0]);
-        int[] moves = parseMoves(split[1]);
-
-        int robotX = -1;
-        int robotY = -1;
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == '@') {
-                    robotX = x;
-                    robotY = y;
-                }
-            }
-        }
+        char[][] map = parseWideMap(split[0]);
+        char[] moves = split[1].replaceAll("\n", "").toCharArray();
+        int[] robot = locateRobot(map);
+        int robotX = robot[0];
+        int robotY = robot[1];
 
         for (int dir : moves) {
             if (canMove(map, robotX, robotY, dir)) {
@@ -104,56 +74,79 @@ public class Day15 extends Day {
             }
         }
 
-        int result = 0;
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == '[') {
-                    result += y * 100 + x;
-                }
-            }
-        }
+        int result = sumUpCoordinates(map, '[');
 
         return String.valueOf(result);
     }
 
-    private int[][] parseMap(String mapString) {
-        return mapString.lines().map(s -> s.chars().toArray()).toArray(int[][]::new);
-    }
-
-    private int[][] parseWideMap(String mapString) {
-        return mapString.lines().map(line -> line.chars()
-            .flatMap(c -> switch (c) {
-                    case '#' -> IntStream.of('#', '#');
-                    case '.' -> IntStream.of('.', '.');
-                    case 'O' -> IntStream.of('[', ']');
-                    case '@' -> IntStream.of('@', '.');
-                    default -> throw new IllegalStateException("Unexpected value: " + c);
+    private int sumUpCoordinates(char[][] map, char symbol) {
+        int result = 0;
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+                if (map[y][x] == symbol) {
+                    result += y * 100 + x;
                 }
-            ).toArray()
-        ).toArray(int[][]::new);
+            }
+        }
+        return result;
     }
 
-    private int[] parseMoves(String movesString) {
-        return movesString.chars()
-            .filter(c -> c != '\n')
-            .map(c -> switch (c) {
-                case '^' -> UP;
-                case '>' -> RIGHT;
-                case 'v' -> DOWN;
-                case '<' -> LEFT;
-                default -> throw new IllegalStateException("Unexpected value: " + c);
-            }).toArray();
+    private char[][] parseMap(String mapString) {
+        return mapString.lines().map(String::toCharArray).toArray(char[][]::new);
+    }
+
+    private char[][] parseWideMap(String mapString) {
+        char[][] map = parseMap(mapString);
+        char[][] wideMap = new char[map.length][map[0].length * 2];
+
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+                switch (map[y][x]) {
+                    case '#':
+                        wideMap[y][x * 2] = '#';
+                        wideMap[y][x * 2 + 1] = '#';
+                        break;
+                    case '.':
+                        wideMap[y][x * 2] = '.';
+                        wideMap[y][x * 2 + 1] = '.';
+                        break;
+                    case 'O':
+                        wideMap[y][x * 2] = '[';
+                        wideMap[y][x * 2 + 1] = ']';
+                        break;
+                    case '@':
+                        wideMap[y][x * 2] = '@';
+                        wideMap[y][x * 2 + 1] = '.';
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + map[y][x]);
+                }
+            }
+        }
+
+        return wideMap;
+    }
+
+    private int[] locateRobot(char[][] map) {
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+                if (map[y][x] == '@') {
+                    return new int[]{x, y};
+                }
+            }
+        }
+        return new int[]{-1, -1};
     }
 
     private int nextX(int x, int dir) {
-        return x + (dir == RIGHT ? 1 : dir == LEFT ? -1 : 0);
+        return x + (dir == '>' ? 1 : dir == '<' ? -1 : 0);
     }
 
     private int nextY(int y, int dir) {
-        return y + (dir == DOWN ? 1 : dir == UP ? -1 : 0);
+        return y + (dir == 'v' ? 1 : dir == '^' ? -1 : 0);
     }
 
-    private boolean canMove(int[][] map, int x, int y, int dir) {
+    private boolean canMove(char[][] map, int x, int y, int dir) {
         int x2 = nextX(x, dir);
         int y2 = nextY(y, dir);
 
@@ -161,26 +154,26 @@ public class Day15 extends Day {
             return true;
         }
 
-        if (dir == UP || dir == DOWN) {
+        if (dir == '^' || dir == 'v') {
             if (map[y2][x2] == '[') {
                 return canMove(map, x2, y2, dir) && canMove(map, x2 + 1, y2, dir);
             } else if (map[y2][x2] == ']') {
                 return canMove(map, x2 - 1, y2, dir) && canMove(map, x2, y2, dir);
             }
-        } else if (dir == RIGHT && map[y2][x2] == '[') {
+        } else if (dir == '>' && map[y2][x2] == '[') {
             return canMove(map, x2 + 1, y2, dir);
-        } else if (dir == LEFT && map[y2][x2] == ']') {
+        } else if (dir == '<' && map[y2][x2] == ']') {
             return canMove(map, x2 - 1, y2, dir);
         }
 
         return false;
     }
 
-    private void move(int[][] map, int x, int y, int dir) {
+    private void move(char[][] map, int x, int y, int dir) {
         int x2 = nextX(x, dir);
         int y2 = nextY(y, dir);
 
-        if (dir == UP || dir == DOWN) {
+        if (dir == '^' || dir == 'v') {
             if (map[y2][x2] == '[') {
                 move(map, x2, y2, dir);
                 move(map, x2 + 1, y2, dir);
@@ -188,10 +181,10 @@ public class Day15 extends Day {
                 move(map, x2 - 1, y2, dir);
                 move(map, x2, y2, dir);
             }
-        } else if (dir == RIGHT && map[y2][x2] == '[') {
+        } else if (dir == '>' && map[y2][x2] == '[') {
             move(map, x2 + 1, y2, dir);
             map[y2][x2 + 1] = map[y2][x2];
-        } else if (dir == LEFT && map[y2][x2] == ']') {
+        } else if (dir == '<' && map[y2][x2] == ']') {
             move(map, x2 - 1, y2, dir);
             map[y2][x2 - 1] = map[y2][x2];
         }
