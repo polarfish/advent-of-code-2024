a1df861e6aa2c78d384987dadee56cf5f02d19a2
Eugene Grybinnyk <eugeneg@backbase.com>
2024-12-16 18:58:42 +0100

    Day 16 (final refactoring)

--- src/main/java/Day16.java
+++ src/main/java/Day16.java
@@ -30,77 +30,30 @@ public class Day16 extends Day {
         day.run(full, day::part2, "Part 2 result");
     }
 
-    public static final int X = 0;
-    public static final int Y = 1;
-    public static final int DIR = 2;
-    public static final int SCORE = 3;
-
     @Override
     public String part1(String input) {
-        char[][] map = input.lines().map(String::toCharArray).toArray(char[][]::new);
-
-        Queue<int[]> paths = new PriorityQueue<>(input.length(), Comparator.comparing(step -> step[SCORE]));
-        Set<Integer> visited = new HashSet<>();
-
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == 'S') {
-                    paths.add(new int[]{x, y, 1, 0, 1});
-                    break;
-                }
-            }
-        }
-
-        int[] s = null;
-        while (!paths.isEmpty()) {
-            s = paths.poll();
-            int x = s[X];
-            int y = s[Y];
-            int dir = s[DIR];
-            visited.add(State.directedCoordId(x, y, dir));
-            int score = s[SCORE];
-            if (map[y][x] == 'E') {
-                break;
-            }
-
-            int x2, y2, dir2;
-
-            // forward
-            x2 = nextX(x, dir);
-            y2 = nextY(y, dir);
-            if (map[y2][x2] != '#' && !visited.contains(State.directedCoordId(x2, y2, dir))) {
-                paths.add(new int[]{x2, y2, dir, score + 1});
-            }
-
-            // right
-            dir2 = RIGHT_TURN[dir];
-            if (map[y][x] != '#' && !visited.contains(State.directedCoordId(x, y, dir2))) {
-                paths.add(new int[]{x, y, dir2, score + 1000});
-            }
-
-            // left
-            dir2 = LEFT_TURN[dir];
-            if (map[y][x] != '#' && !visited.contains(State.directedCoordId(x, y, dir2))) {
-                paths.add(new int[]{x, y, dir2, score + 1000});
-            }
-        }
-
-        int result = s != null ? s[SCORE] : 0;
-
+        int result = solve(input, true);
         return String.valueOf(result);
     }
 
     @Override
     public String part2(String input) {
+        int result = solve(input, false);
+        return String.valueOf(result);
+    }
+
+    private int solve(String input, boolean returnFirstBestPath) {
         char[][] map = input.lines().map(String::toCharArray).toArray(char[][]::new);
 
-        Queue<State> paths = new PriorityQueue<>(input.length(), Comparator.comparing(State::score));
+        Queue<State> paths = new PriorityQueue<>(Comparator.comparing(State::score));
 
-        Map<Integer, Integer> visited = new HashMap<>();
+        Map<Integer, Integer> visited = new HashMap<>(input.length());
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[0].length; x++) {
                 if (map[y][x] == 'S') {
-                    paths.add(new State(x, y, 3, 0, null));
+                    State startingState = new State(x, y, 1, 0, null);
+                    paths.add(startingState);
+                    visited.put(startingState.directedCoordId(), 0);
                     break;
                 }
             }
@@ -110,72 +63,94 @@ public class Day16 extends Day {
         Set<Integer> allBestPathsVisited = new HashSet<>();
         while (!paths.isEmpty()) {
             State s = paths.poll();
-            int x = s.x();
-            int y = s.y();
-            int dir = s.dir();
-            int score = s.score();
 
-            if (map[y][x] == 'E') {
-                if (bestScore == -1 || bestScore == score) {
-                    bestScore = score;
-                    State curr = s;
-                    while (curr != null) {
-                        allBestPathsVisited.add(curr.coordId());
-                        curr = curr.prev();
+            if (map[s.y()][s.x()] == 'E') {
+                if (bestScore == -1 || bestScore == s.score()) {
+                    bestScore = s.score();
+                    if (returnFirstBestPath) {
+                        return s.score();
+                    } else {
+                        State curr = s;
+                        while (curr != null) {
+                            allBestPathsVisited.add(curr.coordId());
+                            curr = curr.prev();
+                        }
                     }
                 }
                 continue;
             }
 
-            int x2, y2, dir2, score2;
-            State newState;
-
-            // forward
-            x2 = nextX(x, dir);
-            y2 = nextY(y, dir);
-            score2 = score + 1;
-            if (map[y2][x2] != '#') {
-                Integer visitedScore = visited.get(State.directedCoordId(x2, y2, dir));
-                if (visitedScore == null || visitedScore >= score2) {
-                    paths.add(newState = new State(x2, y2, dir, score2, s));
-                    visited.put(newState.directedCoordId(), score2);
-                }
+            if (s.canGoForward(map, visited)) {
+                processState(s.goForward(), visited, paths);
             }
-
-            // right
-            dir2 = RIGHT_TURN[dir];
-            score2 = score + 1000;
-            if (map[y][x] != '#') {
-                Integer visitedScore = visited.get(State.directedCoordId(x, y, dir2));
-                if (visitedScore == null || visitedScore >= score2) {
-                    paths.add(newState = new State(x, y, dir2, score2, s));
-                    visited.put(newState.directedCoordId(), score2);
-                }
+            if (s.canTurnRight(visited)) {
+                processState(s.turnRight(), visited, paths);
             }
-
-            // left
-            dir2 = LEFT_TURN[dir];
-            score2 = score + 1000;
-            if (map[y][x] != '#') {
-                Integer visitedScore = visited.get(State.directedCoordId(x, y, dir2));
-                if (visitedScore == null || visitedScore >= score2) {
-                    paths.add(newState = new State(x, y, dir2, score2, s));
-                    visited.put(newState.directedCoordId(), score2);
-                }
+            if (s.canTurnLeft(visited)) {
+                processState(s.turnLeft(), visited, paths);
             }
         }
 
-        return String.valueOf(allBestPathsVisited.size());
+        return allBestPathsVisited.size();
+    }
 
+    private static void processState(State s2, Map<Integer, Integer> visited, Queue<State> paths) {
+        paths.add(s2);
+        visited.put(s2.directedCoordId(), s2.score());
     }
 
     record State(int x, int y, int dir, int score, State prev) {
 
-        public int coordId() {
-            return coordId(x, y);
+        public static final int[] RIGHT_TURN = new int[]{1, 2, 3, 0};
+        public static final int[] LEFT_TURN = new int[]{3, 0, 1, 2};
+
+        public boolean canGoForward(char[][] map, Map<Integer, Integer> visited) {
+            int x2 = nextX();
+            int y2 = nextY();
+            if (map[y2][x2] == '#') {
+                return false;
+            }
+            int directedCoordId = directedCoordId(x2, y2, dir);
+            Integer visitedScore = visited.get(directedCoordId);
+            return visitedScore == null || visitedScore >= score + 1;
         }
 
-        public int coordId(int x, int y) {
+        public State goForward() {
+            return new State(nextX(), nextY(), dir, score + 1, this);
+        }
+
+        public boolean canTurnRight(Map<Integer, Integer> visited) {
+            int dir2 = RIGHT_TURN[dir];
+            int directedCoordId = directedCoordId(x, y, dir2);
+            Integer visitedScore = visited.get(directedCoordId);
+            return visitedScore == null || visitedScore >= score + 1000;
+        }
+
+        public State turnRight() {
+            return new State(x, y, RIGHT_TURN[dir], score + 1000, this);
+        }
+
+        public boolean canTurnLeft(Map<Integer, Integer> visited) {
+            int dir2 = LEFT_TURN[dir];
+            int directedCoordId = directedCoordId(x, y, dir2);
+            Integer visitedScore = visited.get(directedCoordId);
+            return visitedScore == null || visitedScore >= score + 1000;
+        }
+
+
+        public State turnLeft() {
+            return new State(x, y, LEFT_TURN[dir], score + 1000, this);
+        }
+
+        public int nextX() {
+            return x + (dir == 1 ? 1 : dir == 3 ? -1 : 0);
+        }
+
+        public int nextY() {
+            return y + (dir == 2 ? 1 : dir == 0 ? -1 : 0);
+        }
+
+        public int coordId() {
             return x * 1000 + y;
         }
 
@@ -183,21 +158,10 @@ public class Day16 extends Day {
             return directedCoordId(x, y, dir);
         }
 
-        public static int directedCoordId(int x, int y, int dir) {
+        private static int directedCoordId(int x, int y, int dir) {
             return x * 10_000 + y * 10 + dir;
         }
+
     }
-
-    public static final int[] RIGHT_TURN = new int[]{1, 2, 3, 0};
-    public static final int[] LEFT_TURN = new int[]{3, 0, 1, 2};
-
-    public int nextX(int x, int dir) {
-        return x + (dir == 1 ? 1 : dir == 3 ? -1 : 0);
-    }
-
-    public int nextY(int y, int dir) {
-        return y + (dir == 2 ? 1 : dir == 0 ? -1 : 0);
-    }
-
 
 }
